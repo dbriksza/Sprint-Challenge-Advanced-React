af6c0dda95ef08514912f7ccb2aa932bce6c1046
dbriksza <dbriksza@gmail.com>
2019-10-04 12:47:27 -0400

    MVP

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const PlayerCard = props => {
   return (
-    <div className={`playercard ${props.country}`} id={props.id}>
-      <h3>{props.name}</h3>
-      <h4>{props.country}</h4>
-    </div>
+    <a href={`https://en.wikipedia.org/wiki/${props.name.replace(/ /g, "_")}`}>
+      <div className={`playercard ${props.country}`} id={props.id}>
+        <h3>{props.name}</h3>
+        <h4>{props.country}</h4>
+      </div>
+    </a>
   );
 };
 
